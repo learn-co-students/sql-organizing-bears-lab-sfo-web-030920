bc3772fc543769d44a116b2d4b9b5adcddfac7dc
Ty Bandachit <tbandachit@gmail.com>
2020-03-18 18:42:06 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,8 @@
+CREATE TABLE bears (
+id INTEGER primary key,
+name TEXT,
+age integer,
+gender text,
+color text,
+temperament text,
+alive BOOLEAN)
---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name, age, gender, color, temperament)
+VALUES("Mr.Chocolate", 20, "M", "Black", "Angry"),
+("Rowdy", 20, "M", "Black", "Angry"),
+("Tabitha", 20, "F", "Black", "Angry"),
+("Sergeant Brown", 20, "M", "Black", "Angry"),
+("Melissa", 20, "F", "Black", "Angry"),
+("Grinch", 20, "M", "Black", "Angry"),
+("Wendy", 20, "F", "Black", "Angry"),
+(NULL, 20, "M", "Black", "Angry");
